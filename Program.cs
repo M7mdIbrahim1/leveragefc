aafb9061edf98771a4b3747f728376245775095b
M7mdIbrahim1 <66166461+M7mdIbrahim1@users.noreply.github.com>
2023-09-20 18:26:14 +0300

    first publish

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 builder.Services.AddCors(options =>
              {
                  options.AddPolicy(name: "ProdOrigins", builder =>
-                      builder.WithOrigins("https://3.71.175.227/", "http://3.71.175.227/")
+                      builder.WithOrigins("https://3.75.83.162/", "http://3.75.83.162/")
                       .AllowAnyHeader()
                       .AllowAnyMethod()
                       .AllowCredentials());
