696835bb236a9a8731fb4b52186bd86b49465227
M7mdIbrahim1 <66166461+M7mdIbrahim1@users.noreply.github.com>
2023-09-23 21:05:34 +0300

    localhost cors publish

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 builder.Services.AddCors(options =>
              {
                  options.AddPolicy(name: "ProdOrigins", builder =>
-                      builder.WithOrigins("https://3.75.83.162/", "http://3.75.83.162/")
+                      builder.WithOrigins("https://localhost/", "http://localhost/")
                       .AllowAnyHeader()
                       .AllowAnyMethod()
                       .AllowCredentials());
